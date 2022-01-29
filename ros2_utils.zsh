0e30e8bcfdc07e2820a8344e599d084e5ad04b5d
Tony Najjar <tony.najjar@logivations.com>
2022-01-29 01:51:59 +0100

    Added zsh

--- ros2_utils.zsh
+++ ros2_utils.zsh
@@ -1,53 +1,53 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
 # Topics
 
 function rtlist {
-    CMD="ros2 topic list"
+    CMD=(ros2 topic list)
     echo $CMD
     $CMD
-    history -s rtlist
-    history -s $CMD
+    print -s rtlist
+    print -s $CMD
 }
 
 function rtecho {
     TOPIC=$(ros2 topic list | fzf)
-    CMD="ros2 topic echo $TOPIC"
+    CMD=(ros2 topic echo $TOPIC)
     echo $CMD
     $CMD
-    history -s rtecho
-    history -s $CMD
+    print -s rtecho
+    print -s $CMD
 }
 
 function rtinfo {
     TOPIC=$(ros2 topic list | fzf)
-    CMD="ros2 topic info -v $TOPIC"
+    CMD=(ros2 topic info -v $TOPIC)
     echo $CMD
     $CMD
-    history -s rtinfo
-    history -s $CMD
+    print -s rtinfo
+    print -s $CMD
 }
 
 # Nodes
 
 function rnlist {
-    CMD="ros2 node list"
+    CMD=(ros2 node list)
     echo $CMD
     $CMD
-    history -s rnlist
-    history -s $CMD
+    print -s rnlist
+    print -s $CMD
 }
 
 function rninfo {
     NODE=$(ros2 node list | fzf)
-    CMD="ros2 node info $NODE"
+    CMD=(ros2 node info $NODE)
     echo $CMD
     $CMD
-    history -s rninfo
-    history -s $CMD
+    print -s rninfo
+    print -s $CMD
 }
 
-
+# TODO: Not working
 function rnkill {
     NODE_TO_KILL_RAW=$(ros2 node list | fzf)
     NODE_TO_KILL=(${NODE_TO_KILL_RAW//// })
@@ -66,51 +66,46 @@ function rnkill {
         return
     fi
     PROC_PID=$(ps aux | grep [/]$NODE_TO_KILL | awk '{print $2}')
-    CMD="kill $PROC_PID"
+    CMD=(kill $PROC_PID)
     echo "Killing $NODE_TO_KILL_RAW with PID $PROC_PID"
     $CMD
-    history -s rnlist
-    history -s $CMD
+    print -s rnlist
+    print -s $CMD
 }
 
 # TF
 
 function view_frames {
     if [ $# -eq 0 ]; then
-        REMAP=""
+        echo "ros2 run tf2_tools view_frames"
+        ros2 run tf2_tools view_frames
+        print -s ros2 run tf2_tools view_frames
     else
-        REMAP="--ros-args -r /tf:=/$1/tf -r /tf_static:=/$1/tf_static"
+        echo "ros2 run tf2_tools view_frames --ros-args -r /tf:=/$1/tf -r /tf_static:=/$1/tf_static"
+        ros2 run tf2_tools view_frames --ros-args -r /tf:=/$1/tf -r /tf_static:=/$1/tf_static
+        print -s run tf2_tools view_frames --ros-args -r /tf:=/$1/tf -r /tf_static:=/$1/tf_static
     fi
-    CMD="ros2 run tf2_tools view_frames $REMAP"
-    echo $CMD
-    $CMD
-    history -s view_frames $@
-    history -s $CMD
+    print -s view_frames $@
 }
 
 function tf_echo {
     if [ $# -eq 3 ]; then
-        REMAP="--ros-args -r /tf:=/$3/tf -r /tf_static:=/$3/tf_static"
+        echo "ros2 run tf2_ros tf2_echo $1 $2 --ros-args -r /tf:=/$3/tf -r /tf_static:=/$3/tf_static"
+        ros2 run tf2_ros tf2_echo $1 $2 --ros-args -r /tf:=/$3/tf -r /tf_static:=/$3/tf_static
     else
-        REMAP=""
+        echo "ros2 run tf2_ros tf2_echo $1 $2"
+        ros2 run tf2_ros tf2_echo $1 $2
     fi
-    CMD="ros2 run tf2_ros tf2_echo $1 $2 $REMAP"
-    echo $CMD
-    $CMD
-    history -s tf_echo $@
-    history -s $CMD
+    print -s tf_echo $@
 }
 
 # Colcon
 
 function cb {
     if [ $# -eq 0 ]; then
-        CMD="colcon build --symlink-install"
+        colcon build --symlink-install
     else
-        CMD="colcon build --symlink-install --packages-select $@"
+        colcon build --symlink-install --packages-select $@
     fi
-    echo $CMD
-    $CMD
-    history -s cb $@
-    history -s $CMD
+    print -s cb $@
 }
